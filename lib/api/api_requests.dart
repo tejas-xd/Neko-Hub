c60f808f705cbec0bd258ee133b215bf2263208d
tejas-xd <tskakde@gmail.com>
2023-01-10 21:04:37 +0530

    added function to call data for detail screen
    Signed-off-by: tejas-xd <tskakde@gmail.com>

--- lib/api/api_requests.dart
+++ lib/api/api_requests.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:weeb_hub/api/model/infomodel.dart';
 import 'model/model.dart';
 
 class APIService {
@@ -37,6 +38,20 @@ class APIService {
       var anime = response.data['results'] as List;
       List<Results> animeList = anime.map((m) => Results.fromJson(m)).toList();
       return animeList;
+    } catch (error, stacktrace) {
+      throw Exception(
+          'Exception accoured: $error with stacktrace: $stacktrace');
+    }
+  }
+
+
+  Future<AnimeInfo> getinfo(String info) async {
+    try {
+      final url = '$baseUrl/meta/anilist/info/$info';
+      final response = await _dio.get(url);
+      var anime = response.data;
+      AnimeInfo animeinfo = AnimeInfo.fromJson(anime);
+      return animeinfo;
     } catch (error, stacktrace) {
       throw Exception(
           'Exception accoured: $error with stacktrace: $stacktrace');
