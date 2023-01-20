ecccdffa23305e3f7888d81a8dc3ee61a75bb899
tejas-xd <tskakde@gmail.com>
2023-01-21 02:20:47 +0530

    edited more exports to screens
    Signed-off-by: tejas-xd <tskakde@gmail.com>

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,9 @@
 export 'package:weeb_hub/screens/start/loginscreen.dart';
 export 'package:weeb_hub/screens/profile/profilescreen.dart';
 export 'package:weeb_hub/screens/start/startscreen.dart';
-export 'list/listscreen.dart';
-export 'home/homescreen.dart';
-export 'download/downloadscreen.dart';
-export 'calender/calenderscreen.dart';
+export 'package:weeb_hub/screens/list/listscreen.dart';
+export 'package:weeb_hub/screens/home/homescreen.dart';
+export 'package:weeb_hub/screens/download/downloadscreen.dart';
+export 'package:weeb_hub/screens/calender/calenderscreen.dart';
+export 'package:weeb_hub/screens/search/SearchScreen.dart';
+export 'package:weeb_hub/screens/detail/DetailScreen.dart';
