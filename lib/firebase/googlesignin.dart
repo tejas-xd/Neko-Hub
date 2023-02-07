e56f7d57a4096da2f9b2a38b5ee87819dad1b257
tejas-xd <tskakde@gmail.com>
2023-02-07 21:44:19 +0530

    added database instance
    Signed-off-by: tejas-xd <tskakde@gmail.com>

--- lib/firebase/googlesignin.dart
+++ lib/firebase/googlesignin.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -16,7 +17,21 @@ class GoogleSignInProvider extends ChangeNotifier {
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
-    await FirebaseAuth.instance.signInWithCredential(credential);
+    await FirebaseAuth.instance.signInWithCredential(credential).then((value) => {
+      FirebaseFirestore.instance.collection("Users").doc(FirebaseAuth.instance.currentUser!.uid).set({
+        'name' : FirebaseAuth.instance.currentUser!.displayName,
+        'Email' : FirebaseAuth.instance.currentUser!.email
+      }),
+      FirebaseFirestore.instance.collection("Users").doc(FirebaseAuth.instance.currentUser!.uid).collection('movies').doc(FirebaseAuth.instance.currentUser!.uid).set({
+        'movies' : FieldValue.arrayUnion([])
+      }),
+      FirebaseFirestore.instance.collection("Users").doc(FirebaseAuth.instance.currentUser!.uid).collection('series').doc(FirebaseAuth.instance.currentUser!.uid).set({
+        'series' : FieldValue.arrayUnion([])
+      }),
+      FirebaseFirestore.instance.collection("Users").doc(FirebaseAuth.instance.currentUser!.uid).collection('watched').doc(FirebaseAuth.instance.currentUser!.uid).set({
+        'watched' : FieldValue.arrayUnion([])
+      }),
+    });
     notifyListeners();
   }
 
